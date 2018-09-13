237216241996cf21d6d10ee0a28141e819d84d11
WINDOWS 10 <WINDOWS 10@DESKTOP-VQO2UIQ>
2018-09-13 13:45:42 +0700

    Initial Commit

---
+++ src/menghitungfibonacci/MenghitungFibonacci.java
@@ -0,0 +1,19 @@
+package menghitungfibonacci;
+
+import java.util.Scanner;
+public class MenghitungFibonacci {
+
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        
+    } 
+    public static void main(String[] args) {
+        
+        String identitas = "Fara Nisha Sukma Gustika / XRPL2 / 14";
+        tampilJudul(identitas);
+
+}
+}
